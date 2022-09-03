5cc60a3037759b606e8e1835b9c934efc8177ef5
gyeongman <rudaks012@naver.com>
2022-09-03 19:18:23 +0900

    Refactor : 리팩토링

--- src/main/java/firststep/StringPlusCalculator.java
+++ src/main/java/firststep/StringPlusCalculator.java
@@ -1,16 +1,19 @@
 package firststep;
 
+import java.util.Arrays;
+
 public class StringPlusCalculator {
+
+    public static final String DELIMITER = "[,|:]";
+
     public int splitAndSum(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        String[] strings = input.split("[,|:]");
-        int sum = 0;
-        for (String string : strings) {
-            int parseInt = Integer.parseInt(string);
-            sum += parseInt;
-        }
-        return sum;
+        String[] splitStrings = input.split(DELIMITER);
+
+        return Arrays.stream(splitStrings)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
