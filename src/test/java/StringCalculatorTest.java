429211c4570c21233ecbb1f5fc0902837817dbe1
gyeongman <rudaks012@naver.com>
2022-09-03 20:12:09 +0900

    Refactor : 리팩토링

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -11,20 +11,28 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class StringCalculatorTest {
     private final StringPlusCalculator calculator = new StringPlusCalculator();
 
+    private int actual;
+    private int excepted;
+
     @DisplayName("계산기 입력값에 null 또는 음수가 들어오면 0을 반환한다")
     @ParameterizedTest(name = "계산기 입력값에 null 또는 음수가 들어오면 0을 반환한다")
     @NullAndEmptySource
     void calculator_insert_null_or_empty_return_zero(String input) {
-        int result = calculator.splitAndSum(input);
-        assertThat(result).isEqualTo(0);
+        actual = calculator.splitAndSum(input);
+
+        excepted = 0;
+
+        assertActual(actual, excepted);
     }
 
     @Test
     @DisplayName("계산기 입력값에 숫자하나만 입력이 될 경우")
     void calculator_only_one_number() {
-        int result = calculator.splitAndSum("1");
+        actual = calculator.splitAndSum("1");
 
-        assertThat(result).isEqualTo(1);
+        excepted = 1;
+
+        assertActual(actual, excepted);
     }
 
     @DisplayName("계산기 입력값은 쉼표 구분자로 나뉘고 더해진다")
@@ -34,9 +42,11 @@ public class StringCalculatorTest {
             "2,1"
     })
     void calculator_comma_separator_sum(String input) {
-        int actual = calculator.splitAndSum(input);
+        actual = calculator.splitAndSum(input);
 
-        assertThat(actual).isEqualTo(3);
+        excepted = 3;
+
+        assertActual(actual, excepted);
     }
 
     @DisplayName("계산기 입력값은 콤마 또는 콜론으로 구분된다.")
@@ -46,17 +56,22 @@ public class StringCalculatorTest {
             "1:2,3"
     })
     void calculator_comma_or_colon_separator_sum(String input) {
-        int actual = calculator.splitAndSum(input);
+        actual = calculator.splitAndSum(input);
 
-        assertThat(actual).isEqualTo(6);
+        excepted = 6;
+
+        assertActual(actual, excepted);
     }
+
 
     @Test
     @DisplayName("계산기 입력 값에 커스텀 구분자로 덧셈한다")
     void calculator_custom_separator_insert_is_sum() {
-        int actual = calculator.splitAndSum("//;\n1;2;3");
+        actual = calculator.splitAndSum("//;\n1;2;3");
 
-        assertThat(actual).isEqualTo(6);
+        excepted = 6;
+
+        assertActual(actual, excepted);
     }
 
     @ParameterizedTest(name = "음수가 입력되면 예외가 발생한다.")
@@ -72,5 +87,8 @@ public class StringCalculatorTest {
                 .hasMessage("음수가 발생하면 안됩니다");
     }
 
+    private static void assertActual(int actual, int excepted) {
+        assertThat(actual).isEqualTo(excepted);
+    }
 
 }
