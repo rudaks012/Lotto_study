ba62b63212c19956349806004786462c80324283
gyeongman <rudaks012@naver.com>
2022-09-03 23:57:09 +0900

    Feat : 로또 금액 예외처리

--- src/main/java/secondstep/InputView.java
+++ src/main/java/secondstep/InputView.java
@@ -6,12 +6,14 @@ import java.util.Scanner;
 public class InputView {
 
     public static final String PRINT_BUY_MESSAGE = "구입 금액을 입력해 주세요.";
-    public static final String LAST_WEEK_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     public static final Scanner SCANNER = new Scanner(System.in);
 
     public static final Random RANDOM = new Random();
     public static final int LOTTO_ONE_PRICE_AMOUNT = 1000;
     public static final String PRINT_BUY_COUNT = "개를 구매 했습니다.";
+    public static final int LOTTO_MAX_COUNT = 6;
+    public static final int ZERO = 0;
+    public static final int REMAINDER_VALUE = 10;
 
 
     public static int printBuyMessage() {
@@ -19,21 +21,36 @@ public class InputView {
         return SCANNER.nextInt();
     }
 
-    public int  lottoBuyAmount() {
+    public void lottoBuyAmount() {
         int amount = printBuyMessage();
-        int totalBuyQuantity = amountToQuantity(amount);
+        amountExceptionValidition(amount);
+        int totalBuyQuantity = getTotalBuyQuantity(amount);
         System.out.println(totalBuyQuantity + PRINT_BUY_COUNT);
-        return totalBuyQuantity;
+    }
+
+    private static void amountExceptionValidition(int amount) {
+        if (amount < LOTTO_ONE_PRICE_AMOUNT) {
+            throw new IllegalArgumentException("돈이 천원 보다 작습니다.");
+        }
+        if (amount % REMAINDER_VALUE != ZERO) {
+            throw new IllegalArgumentException("천원 단위로 입력 해야 합니다.");
+        }
+    }
+
+    private int getTotalBuyQuantity(int amount) {
+        return amountToQuantity(amount);
     }
 
     private int amountToQuantity(int amount) {
         return amount / LOTTO_ONE_PRICE_AMOUNT;
     }
 
+    public void lottoNumberGeneration() {
+        int[] lottoNumber = new int [LOTTO_MAX_COUNT];
 
-    public static String[] printLastWeekWinningNumber() {
-        System.out.println(LAST_WEEK_MESSAGE);
-        return SCANNER.nextLine().split(",");
+        for (int i = 0; i < LOTTO_MAX_COUNT; i++) {
+            lottoNumber[i] = (int)(Math.random() * 45) + 1;
+
+        }
     }
-
 }
--- src/main/java/secondstep/ResultView.java
+++ src/main/java/secondstep/ResultView.java
@@ -1,4 +1,13 @@
 package secondstep;
 
+import java.util.Scanner;
+
 public class ResultView {
+    public static final String LAST_WEEK_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static String[] printLastWeekWinningNumber() {
+        System.out.println(LAST_WEEK_MESSAGE);
+        return SCANNER.nextLine().split(",");
+    }
 }
