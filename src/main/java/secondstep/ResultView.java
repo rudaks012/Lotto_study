b581ead1f0b3ceb67afd88ad1a4cc7b650f36e77
gyeongman <rudaks012@naver.com>
2022-09-04 15:53:14 +0900

    Feat : 당첨 통계 조회

--- src/main/java/secondstep/ResultView.java
+++ src/main/java/secondstep/ResultView.java
@@ -1,15 +1,13 @@
 package secondstep;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class ResultView {
     public static final String LAST_WEEK_WINNER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     public static final Scanner SCANNER = new Scanner(System.in);
 
-    public List<int[]> resultViewStart() {
+    public int[] resultViewStart() {
         String[] winnerNumbers = printLastWeekWinningNumber();
         return getLastWeekWinnerNumbers(winnerNumbers);
     }
@@ -19,9 +17,8 @@ public class ResultView {
         return SCANNER.nextLine().split(",");
     }
 
-    private static List<int[]> getLastWeekWinnerNumbers(String[] winnerNumbers) {
-        int[] parseInt = Arrays.stream(winnerNumbers).mapToInt(Integer::parseInt).toArray();
-        return Collections.singletonList(parseInt);
+    private static int[] getLastWeekWinnerNumbers(String[] winnerNumbers) {
+        return Arrays.stream(winnerNumbers).mapToInt(Integer::parseInt).toArray();
     }
 
 
--- src/main/java/secondstep/WinnerLotto.java
+++ src/main/java/secondstep/WinnerLotto.java
@@ -1,17 +1,54 @@
 package secondstep;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class WinnerLotto {
+    public static final int LOTTO_NUMBER_THREE_MATCH = 3;
+    public static final int LOTTO_NUMBER_FOUR_MATCH = 4;
+    public static final int LOTTO_NUMBER_FIVE_MATCH = 5;
+    public static final int LOTTO_NUMBER_SIX_MATCH = 6;
+    private int three = 0;
+    private int four = 0;
+    private int five = 0;
+    private int six = 0;
+
     public void winnerLottoGame() {
         InputView inputView = new InputView();
-        List<List<Integer>> userLottoNumbers =  inputView.lottoGameStart();
-        System.out.println("userLottoNumbers 123123123= " + userLottoNumbers);
+        List<List<Integer>> userLottoNumbers = inputView.lottoGameStart();
 
         ResultView resultView = new ResultView();
-        List<int[]> winnerLottoNumbers = resultView.resultViewStart();
+        int[] winnerLottoNumbers = resultView.resultViewStart();
 
 
+        for (int i = 0; i < userLottoNumbers.size(); i++) {
+            List<Integer> userLottoNumber = userLottoNumbers.get(i);
+            int count = 0;
+            count = getCount(winnerLottoNumbers, userLottoNumber, count);
+
+            prizeStatistics(count);
+        }
     }
+
+    private static int getCount(int[] winnerLottoNumbers, List<Integer> userLottoNumber, int count) {
+        for (int j = 0; j < 6; j++) {
+            if (userLottoNumber.contains(winnerLottoNumbers[j])) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void prizeStatistics(int count) {
+        if (count == LOTTO_NUMBER_THREE_MATCH) {
+            three++;
+        } else if (count == LOTTO_NUMBER_FOUR_MATCH) {
+            four++;
+        } else if (count == LOTTO_NUMBER_FIVE_MATCH) {
+            five++;
+        } else if (count == LOTTO_NUMBER_SIX_MATCH) {
+            six++;
+        }
+    }
+
+
 }
