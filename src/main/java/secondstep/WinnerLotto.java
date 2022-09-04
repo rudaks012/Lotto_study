eb8166bde2a98b4d8969473f0ee6a4169ebc133c
gyeongman <rudaks012@naver.com>
2022-09-04 19:04:29 +0900

    Feat : 총 수익률 출력

--- src/main/java/secondstep/WinnerLotto.java
+++ src/main/java/secondstep/WinnerLotto.java
@@ -7,16 +7,24 @@ public class WinnerLotto {
     public static final int LOTTO_NUMBER_FOUR_MATCH = 4;
     public static final int LOTTO_NUMBER_FIVE_MATCH = 5;
     public static final int LOTTO_NUMBER_SIX_MATCH = 6;
+
+    private static final int FIRST_REWARD = 2000000000;
+    private static final int SECOND_REWARD = 1500000;
+    private static final int THIRD_REWARD = 50000;
+    private static final int FORTH_REWARD = 5000;
     private static int threeMatch;
     private static int fourMatch;
     private static int fiveMatch;
     private static int sixMatch;
 
+    private int amount;
+
     public void winnerLottoGame() {
         InputView inputView = new InputView();
 
-        List<List<Integer>> userLottoNumbers = inputView.lottoGameStart();
+        amount = inputView.getAmount();
 
+        List<List<Integer>> userLottoNumbers = inputView.lottoGameStart(amount);
         ResultView resultView = new ResultView();
         int[] winnerLottoNumbers = resultView.resultViewStart();
 
@@ -57,5 +65,8 @@ public class WinnerLotto {
         System.out.println("4개 일치 (50000원)-" + fourMatch + "개");
         System.out.println("5개 일치 (1500000원)-" + fiveMatch + "개");
         System.out.println("6개 일치 (200000000원)-" + sixMatch + "개");
+
+        double result = ((sixMatch * FIRST_REWARD) + (fiveMatch * SECOND_REWARD) + (fourMatch * THIRD_REWARD) + (threeMatch * FORTH_REWARD)) / amount;
+        System.out.println("총 수익률은 : " + result + "입니다.");
     }
 }
