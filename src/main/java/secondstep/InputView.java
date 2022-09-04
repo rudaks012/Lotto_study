b2f707821af80190ca73e5d14c17d8edea16a9b1
gyeongman <rudaks012@naver.com>
2022-09-04 19:02:54 +0900

    Test : InputView 테스트 코드 작성

--- src/main/java/secondstep/InputView.java
+++ src/main/java/secondstep/InputView.java
@@ -10,49 +10,51 @@ public class InputView {
     public static final int LOTTO_ONE_PRICE_AMOUNT = 1000;
     public static final String PRINT_BUY_COUNT = "개를 구매 했습니다.";
     public static final int ZERO = 0;
-    public static final int REMAINDER_VALUE = 10;
+    public static final int REMAINDER_VALUE = 1000;
     public static final String EXCEPTION_MONEY_IS_LESS_THOUSAND = "돈이 천원 보다 작습니다.";
     public static final String EXCEPTION_THOUSAND_INSERT = "천원 단위로 입력 해야 합니다.";
 
-
-    public List<List<Integer>> lottoGameStart() {
+    public List<List<Integer>> lottoGameStart(int amount) {
         LottoMaker lottoMaker = new LottoMaker();
-        int amount = getAmount();
         int totalBuyQuantity = getTotalBuyQuantity(amount);
         printTotalBuyQuantity(totalBuyQuantity);
 
         return lottoMaker.lottoNumberGeneration(totalBuyQuantity);
     }
 
-    private int getAmount() {
+    public int getAmount() {
         int amount = printBuyMessage();
         amountExceptionValidation(amount);
+        amountExceptionMustThousand(amount);
         return amount;
     }
 
-    private int printBuyMessage() {
+    public int printBuyMessage() {
         System.out.println(PRINT_BUY_MESSAGE);
         return SCANNER.nextInt();
     }
 
-    private void amountExceptionValidation(int amount) {
+    public void amountExceptionValidation(int amount) {
         if (amount < LOTTO_ONE_PRICE_AMOUNT) {
             throw new IllegalArgumentException(EXCEPTION_MONEY_IS_LESS_THOUSAND);
         }
+    }
+
+    public void amountExceptionMustThousand(int amount) {
         if (amount % REMAINDER_VALUE != ZERO) {
             throw new IllegalArgumentException(EXCEPTION_THOUSAND_INSERT);
         }
     }
 
-    private void printTotalBuyQuantity(int totalBuyQuantity) {
+    public void printTotalBuyQuantity(int totalBuyQuantity) {
         System.out.println(totalBuyQuantity + PRINT_BUY_COUNT);
     }
 
-    private int getTotalBuyQuantity(int amount) {
+    public int getTotalBuyQuantity(int amount) {
         return amountToQuantity(amount);
     }
 
-    private int amountToQuantity(int amount) {
+    public int amountToQuantity(int amount) {
         return amount / LOTTO_ONE_PRICE_AMOUNT;
     }
 
