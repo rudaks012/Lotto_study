02beee88549bb749845e8e252a3094216c8394e5
gyeongman <rudaks012@naver.com>
2022-09-06 00:13:36 +0900

    Style : 상수 이름 의미있는 이름 변경

--- src/main/java/secondstep/LottoMaker.java
+++ src/main/java/secondstep/LottoMaker.java
@@ -8,15 +8,15 @@ import java.util.List;
 public class LottoMaker {
 
     public static final int LOTTO_MAX_NUMBER = 45;
-    public static final int LOTTO_FIRST_NUMBER = 0;
-    public static final int LOTTO_LAST_NUMBER = 6;
+    public static final int LOTTO_FIRST_NUMBER = 1;
+    public static final int SIX_NUMBER_PER_SHEET_LOTTO = 6;
     public static final List<List<Integer>> lottoMaker = new ArrayList<>();
 
     public List<List<Integer>> lottoNumberGeneration(int totalBuyQuantity) {
         for (int i = 1; i <= totalBuyQuantity; i++) {
             List<Integer> lottoNumberList = getLottoNumberList();
             Collections.shuffle(lottoNumberList);
-            lottoMaker.add(lottoNumberList.subList(LOTTO_FIRST_NUMBER, LOTTO_LAST_NUMBER));
+            lottoMaker.add(lottoNumberList.subList(LOTTO_FIRST_NUMBER, SIX_NUMBER_PER_SHEET_LOTTO));
         }
         printLottoNumbers();
         return lottoMaker;
