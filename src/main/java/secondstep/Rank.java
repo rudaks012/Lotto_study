3fd0d175d6b307d501fb436fd19530c73a7e63a5
gyeongman <rudaks012@naver.com>
2022-09-06 18:18:22 +0900

    Feat : 로또 랭크 관련 Enum추가

--- src/main/java/secondstep/Rank.java
+++ src/main/java/secondstep/Rank.java
@@ -1,3 +1,19 @@
 package secondstep;
+
 public enum Rank {
+
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 1_500_000),
+    THIRD(5, false, 50_000),
+    FOURTH(4, false, 1_000),
+    NONE(0, false, 0);
+    private final int matchCount;
+    private final int reward;
+    private final boolean bonusMatched;
+
+    Rank(int matchCount, boolean bonusMatched, int reward ) {
+        this.matchCount = matchCount;
+        this.reward = reward;
+        this.bonusMatched = bonusMatched;
+    }
 }
