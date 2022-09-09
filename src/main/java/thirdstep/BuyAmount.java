6334fa4951024394bb9c089c9e20795f354bda3a
gyeongman <rudaks012@naver.com>
2022-09-09 18:24:03 +0900

    Feat : 금액 관련 커스텀예외, Test코드 작성

--- src/main/java/thirdstep/BuyAmount.java
+++ src/main/java/thirdstep/BuyAmount.java
@@ -1,4 +1,49 @@
 package thirdstep;
 
+import java.util.Objects;
+
 public class BuyAmount {
+    public static final int ZERO = 0;
+    public static final int REMAINDER_VALUE = 1000;
+    public static final int LOTTO_ONE_PRICE_AMOUNT = 1000;
+    private final int amount;
+
+    public BuyAmount(int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    private static void validate(int amount) {
+        validatePriceTicket(amount);
+        validateUnderThousand(amount);
+    }
+
+    private static void validateUnderThousand(int amount) {
+        if (amount % REMAINDER_VALUE != ZERO) {
+            throw new AmountException();
+        }
+    }
+
+    private static void validatePriceTicket(int amount) {
+        if (amount < LOTTO_ONE_PRICE_AMOUNT) {
+            throw new AmountException(amount);
+        }
+    }
+
+    public int getTotalBuyQuantity(int amount) {
+        return amount / LOTTO_ONE_PRICE_AMOUNT;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BuyAmount buyAmount = (BuyAmount) o;
+        return amount == buyAmount.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
 }
---
+++ src/test/java/thirdsteptest/BuyAmountTest.java
@@ -0,0 +1,59 @@
+package thirdsteptest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import thirdstep.AmountException;
+import thirdstep.BuyAmount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+
+public class BuyAmountTest {
+
+    @ParameterizedTest(name = "로또 구매비용이 천원보다 작으면 예외가 발생한다. : [{index}] : [arguments]")
+    @DisplayName("로또 구매비용이 천원보다 작으면 예외가 발생한다.")
+    @ValueSource(ints = {
+            700,
+            800,
+            900
+    })
+    void throw_exception_lotto_buy_cost_less_thousand(int lottoBuyMoney) {
+        final ThrowingCallable actual = () -> new BuyAmount(lottoBuyMoney);
+
+        assertThatThrownBy(actual)
+                .isInstanceOf(AmountException.class)
+                .hasMessage("입력값 " + lottoBuyMoney + "원은 천원 보다 작습니다.");
+    }
+
+    @ParameterizedTest(name = "로또 구매비용은 천원 단위로 입력 해야 합니다. : [{index}] : [{arguments}]")
+    @DisplayName("로또 구매비용은 천원 단위로 입력 해야 합니다.")
+    @ValueSource(ints = {
+            1300,
+            2300,
+            4400
+    })
+    void you_must_enter_in_thousands_of_won(int lottoBuyMoney) {
+        final ThrowingCallable actual = () -> new BuyAmount(lottoBuyMoney);
+
+        assertThatThrownBy(actual)
+                .isInstanceOf(AmountException.class)
+                .hasMessageContaining("천원 단위로 ");
+    }
+    @ParameterizedTest(name = "로또는 한장당 천원이다 : [{index}] : [{arguments}]")
+    @DisplayName("로또는 한장당 천원이다")
+    @CsvSource(value = {
+            "3000, 3",
+            "4000, 4",
+            "2000, 2",
+    })
+    void lotto_ticket_is_thousands_won(int lottoBuyMoney, int excepted) {
+        BuyAmount buyAmount = new BuyAmount(lottoBuyMoney);
+        int actual = buyAmount.getTotalBuyQuantity(lottoBuyMoney);
+
+        assertThat(actual).isEqualTo(excepted);
+    }
+
+}
