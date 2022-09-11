5065aceb819326758420282748790e1a8be0f3ea
gyeongman <rudaks012@naver.com>
2022-09-11 20:03:51 +0900

    Feat : 로또 수동 카운트 추가

--- src/main/java/secondstep/InputView.java
+++ src/main/java/secondstep/InputView.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public static final String PRINT_BUY_COUNT = "개를 구매 했습니다.";
+    public static final String PRINT_BUY_COUNT = "수동으로 %d장, 자동으로 %d개를 구매 했습니다.";
     public static final String LAST_WEEK_WINNER_MESSAGE = "지난 주 당첨 번호를 입력해 주세요.";
     public static final String BONUS_BALL_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
     public static final String MANUAL_BUY_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요.";
-
     public static final String MANUAL_BUY_LOTTO_NUMBER = "수동으로 구매할 번호를 입력해 주세요.";
     public static final Scanner SCANNER = new Scanner(System.in);
 
@@ -18,13 +17,15 @@ public class InputView {
         BuyAmount buyAmount = new BuyAmount();
 
         int totalBuyQuantity = buyAmount.getTotalBuyQuantity(amount);
-        printTotalBuyQuantity(totalBuyQuantity);
+        int manualBuyLottoCount = manualLottoCount();
+
+        printTotalBuyQuantity(totalBuyQuantity, manualBuyLottoCount);
         printLottoNumbers();
         return lottoMaker.lottoNumberGeneration(totalBuyQuantity);
     }
 
-    public void printTotalBuyQuantity(int totalBuyQuantity) {
-        System.out.println(totalBuyQuantity + PRINT_BUY_COUNT);
+    public void printTotalBuyQuantity(int totalBuyQuantity, int manualBuyLottoCount) {
+        System.out.println("수동으로" + manualBuyLottoCount + "장, " + "자동으로 " + (totalBuyQuantity - manualBuyLottoCount) + "개를 구매 했습니다.");
     }
 
     public void printLottoNumbers() {
@@ -35,12 +36,14 @@ public class InputView {
 
     public String[] printLastWeekWinningNumber() {
         System.out.println(LAST_WEEK_WINNER_MESSAGE);
-        return SCANNER.nextLine().split(",");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine().split(",");
     }
 
     public int bonusBallNumber() {
         System.out.println(BONUS_BALL_NUMBER_MESSAGE);
-        return Integer.parseInt(SCANNER.nextLine());
+        Scanner scanner = new Scanner(System.in);
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public int manualLottoCount() {
